bc58204b4c6305a7431bba0609bb87e19781511b
simonlombardi <simon_lombardi@itecriocuarto.org.ar>
2024-04-22 15:11:58 -0300

    finished funcionality

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 const inputTarea = document.getElementById('input-tarea')
 const botonAgregarTarea = document.getElementById('boton-agregar-tarea')
 const listaTareas = document.getElementById("lista-tareas")
-const tareas = []
 
-class Tarea{
-    constructor(id, tarea, estado) {
-        this.id = id,
-        this.tarea = tarea,
-        this.estado = estado
+const agregarTarea = () => {
+    const textoTarea = inputTarea.value
+    if (textoTarea === "") {
     }
+    else{
+        const elementoTarea = document.createElement("li")
+        elementoTarea.innerHTML = textoTarea
+        const botonEliminar = document.createElement("button")
+        botonEliminar.textContent = "X"
+        elementoTarea.appendChild(botonEliminar)
+        listaTareas.appendChild(elementoTarea)
+    }
+    inputTarea.value = ""
 }
+
+listaTareas.addEventListener("click", (e) => {
+    if (e.target.tagName == "LI"){
+        e.target.classList.toggle("realizada")
+    }
+    else if (e.target.tagName == "BUTTON"){
+        e.target.parentElement.remove()
+    }
+})
 
 botonAgregarTarea.addEventListener("click", (e) => {
     e.preventDefault()
-    const textoTarea = inputTarea.value
-    const tarea = new Tarea(Date.now(), textoTarea, estado = "activa")
-    tareas.push(tarea)
-    const elementoTarea = document.createElement("li")
-    const texto = document.createElement("p")
-    texto.textContent = tarea.tarea
-    elementoTarea.appendChild(texto)
-    listaTareas.appendChild(elementoTarea)
+    agregarTarea()
 })
